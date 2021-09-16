8f289cc9b3607baff0548f3aebfea57dfcc1bd98
Wesley <wab0015@mix.wvu.edu>
2021-09-16 00:18:13 -0400

    Added background image, layout component, data bindings for sub headings, fixed cart, alignment, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {TopNavBarComponent} from "./header/top-nav-bar.component";
+import {TopNavBarComponent} from "./nav bar/top-nav-bar.component";
 import {FooterComponent} from "./Footer/footer.component";
 import {CarouselComponent} from "./carousel/carousel.component";
+import {LayoutComponent } from './layout/layout.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
     FooterComponent,
-    CarouselComponent
+    CarouselComponent,
+    LayoutComponent,
+    YoutubeComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent{
+  subtitle1:string ="CANVAS BACKPACKS & RUCKSACKS"
+  subtitle2:string ="CANVAS MESSENGER BAGS & SHOULDER BAGS"
+  subtitle3:string ="CANVAS DUFFLE, OVERNIGHT & TRAVEL BAGS"
+
+
+
+}
